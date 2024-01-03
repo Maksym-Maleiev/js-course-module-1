23758d32491340c8d1d82fa76af703a2f5055e58
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-03 16:08:54 +0200

    Update test-2.js

--- js/test-2.js
+++ js/test-2.js
@@ -4,3 +4,13 @@
 * let username = "Mango";
 * username = "Poly";
 */
+
+let productName = "Droid";
+let pricePerItem = 2000;
+
+productName = "Repair droid";
+pricePerItem = 3500;
+
+console.log(productName);
+
+console.log(pricePerItem);
