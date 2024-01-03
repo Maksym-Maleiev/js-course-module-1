b51b8a8e6958bd2f16532f7dd3156f3635038ba6
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-03 16:13:42 +0200

    Update test-3.js

---
+++ js/test-3.js
@@ -0,0 +1,14 @@
+/*
+// Числа
+const age = 20; const salary = 3710.84;
+// Строки
+const name = "Mango"; const description = "JavaSript essentials";
+// Були
+const isModalOpen = true;
+const isLoggedIn = false;
+*/
+
+const topSpeed = 160;
+
+const distance = 617.54;
+
