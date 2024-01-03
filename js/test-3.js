19d5c1953b75e53112a3da7c040366b69107608e
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-03 16:16:34 +0200

    Update test-3.js

--- js/test-3.js
+++ js/test-3.js
@@ -13,3 +13,9 @@ const distance = 617.54;
 const login = "mango935";
 const isOnline = true;
 const isAdmin = false;
+
+console.log(topSpeed);
+console.log(distance);
+console.log(login);
+console.log(isOnline);
+console.log(isAdmin);
