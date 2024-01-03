c6579f8ee69e1a4926bd243da5266a3dd344edff
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-03 18:43:15 +0200

    Create test-8.js

---
+++ js/test-8.js
@@ -0,0 +1,23 @@
+/*
+// 1. Оголошення параметрів x, y, z
+function multiply(x, y, z) {
+  console.log(`Результат множення дорівню ${x * y * z}`);
+}
+// 2. Передача аргументів
+multiply(2, 3, 5);
+// Результат множення дорівнює 30
+multiply(4, 8, 12);
+// Результат множення дорівнює 384
+multiply(17, 6, 25);
+// Результат множення дорівнює 2550
+*/
+
+// Change code below this line
+function add(a, b, c) {
+  console.log(`Addition result equals ${a + b + c}`);
+  // Change code above this line
+}
+
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
