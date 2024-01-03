e20b3d3e9d17c87bd6c449ad0debc16f11da215a
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-03 18:43:14 +0200

    Update test-7.js

---
+++ js/test-7.js
@@ -0,0 +1,18 @@
+/*
+// 1. Оголошення функції multiply
+function multiply() {
+  // Тіло функції
+  console.log("multiply function invocation");
+}
+// 2. Виклики функції multiply
+multiply();
+// multiply function invocation
+multiply(); // multiply function invocation
+multiply(); // multiply function invocation
+*/
+
+function sayHi() {
+  console.log("Hello, this is my first function!");
+}
+
+sayHi();
