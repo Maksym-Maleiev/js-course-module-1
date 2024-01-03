da306ad006dddcb6f7eb46d883615541c63ea6ff
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-03 19:02:49 +0200

    Update test-9.js

---
+++ js/test-9.js
@@ -0,0 +1,32 @@
+/*
+function multiply(x, y, z) {
+  console.log("Код до return виконується як завжди");
+  // Повертаємо результат вираження множення
+  return x * y * z;
+  console.log("Цей лог ніколи не виконається, він стоїть після return");
+}
+// Результат роботи функції можна зберегти у змінну 
+let result = multiply(2, 3, 5);
+console.log(result);
+// 30
+result = multiply(4, 8, 12);
+console.log(result);
+// 384
+result = multiply(17, 6, 25);
+console.log(result);
+// 2550
+*/
+
+function add(a, b, c) {
+  // Change code below this line
+  return a + b + c;
+
+
+  // Change code above this line
+}
+
+add(2, 5, 8); // 15
+
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
