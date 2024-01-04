13d1e58e57a25412f4c8d946c1ced480bc2eba01
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-04 19:16:29 +0200

    Update test-36.js

---
+++ js/test-36.js
@@ -0,0 +1,22 @@
+function checkForSpam(message) {
+  let result;
+  // Change code below this line
+  const normalized = message.toLowerCase();
+  result = normalized === "spam" || normalized === "sale" ? true : false;
+  // Change code above this line
+  return result;
+}
+
+console.log(checkForSpam("Latest technology news"));
+
+console.log(checkForSpam("JavaScript weekly newsletter"));
+
+console.log(checkForSpam("Get best sale offers now!"));
+
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+
+console.log(checkForSpam("Trust me, this is not a spam message"));
+
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
