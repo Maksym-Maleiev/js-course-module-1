2b517030f6237d491301a924cf35a7e7d0b01c51
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-04 19:16:19 +0200

    Update test-31.js

---
+++ js/test-31.js
@@ -0,0 +1,27 @@
+/*
+const productName = "Repair droid";
+
+console.log(productName[0]);
+// "R"
+console.log(productName[5]);
+// "r"
+console.log(productName[11]);
+// "d"
+console.log(productName[productName.length - 1]);
+// "d"
+*/
+
+const courseTopic = "JavaScript essentials";
+// Change code below this line
+
+const courseTopicLength = courseTopic.length;
+const firstElement = courseTopic[0];
+const lastElement = courseTopic[courseTopic.length - 1];
+
+// Change code above this line
+
+console.log(courseTopicLength);
+
+console.log(firstElement);
+
+console.log(lastElement);
