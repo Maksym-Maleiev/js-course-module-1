5b98ef24c4057b87c3410c17f2c54b7628ac68a4
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-04 11:28:22 +0200

    Update test-19.js

--- js/test-19.js
+++ js/test-19.js
@@ -30,3 +30,17 @@ console.log(cost);
 // 500
 */
 
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if () { // Change this line
+    message =  'Canceled by user!';
+  } else if () { // Change this line
+    message = 'Welcome!';
+  } else {
+    message = 'Access denied, wrong password!';
+  }
+
+  return message;
+}
