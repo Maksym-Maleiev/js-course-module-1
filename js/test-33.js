f1d66a16fa0546de28829154cd224d6bab5365c0
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-04 19:16:23 +0200

    Update test-33.js

---
+++ js/test-33.js
@@ -0,0 +1,21 @@
+function formatMessage(message, maxLength) {
+  let result;
+  // Change code below this line
+
+  result = message.length <= maxLength ? result = message : result = message.slice(0, maxLength) + "...";
+
+  /// Change code above this line
+  return result;
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16));
+
+console.log(formatMessage("Curabitur ligula sapien", 23));
+
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
