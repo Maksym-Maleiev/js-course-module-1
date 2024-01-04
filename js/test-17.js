b8dd1c7aaaadaf620dc3cc8f6ad4dc88fa31715c
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-04 11:10:16 +0200

    Update test-17.js

--- js/test-17.js
+++ js/test-17.js
@@ -6,8 +6,26 @@ console.log(age);
 // 26
 */
 
+/*
 let age = 25;
 // Просто більш короткий запис, працює також
 age += 1;
 console.log(age);
 // 26
+*/
+
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+
+// Change code below this line
+a += 2;
+b -= 4;
+c *= 3;
+d /= 10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
